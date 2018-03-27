f2c3ac7cb4ded9dc4fb327f01bb43faead3203a5
Mick Mason <mickjmason@outlook.com>
2018-03-27 16:29:49 +0100

    added restcontroller to listen on port 8080

--- src/main/java/uk/ltd/skyris/loggenerator/controller/logController.java
+++ src/main/java/uk/ltd/skyris/loggenerator/controller/logController.java
@@ -1,7 +1,15 @@
 package uk.ltd.skyris.loggenerator.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class logController {
-}
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue = "world") String name)
+    {
+        return "hello " + name;
+    }
+        }
